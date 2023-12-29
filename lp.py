7678ecbb713ccd81bf3cca764dcf93b0f1ef4413
Ydupc <65360930+CyberDudeJ@users.noreply.github.com>
2023-12-29 15:14:42 +0000

    Update lp.py

--- lp.py
+++ lp.py
@@ -6,6 +6,23 @@ import sys, os, requests, subprocess, platform
 arg1 = None
 arg2 = None
 
+# Define command execution function
+def exec_cmd(command):
+    stream = os.popen(command)
+    output = stream.read()
+    output
+
+    return output
+
+# Define check if cmd exists function
+def command_exists(command):
+    try:
+        subprocess.check_output(['which', command])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+# Define fetch json file function
 def fetch_json_data(url):
     try:
         response = requests.get(url)
@@ -15,6 +32,7 @@ def fetch_json_data(url):
         print(f"[WARN] Error fetching JSON data {e}, Now exiting.")
         sys.exit(1)
 
+# Define extract values function
 def extract_values(json_data):
     values_list= []
 
@@ -45,8 +63,23 @@ def main(): # Define main function
 
             if json_data is not None:
                 values_list = extract_values(json_data)
-                print("Values list:", values_list)
-
+                print("[INFO] Selected Preset:", values_list)
+                print("[INFO] Some presets require root. Errors may occur if lp is not run as root")
+                index = 0
+                continueBoolean = input("Would you like to continue? [y/n]")
+                if continueBoolean in ["y", "Y"]:
+                    print("[INFO] This may take some time depending on the preset. Please wait")
+                    while index < len(values_list):
+                        current_entry = values_list[index]
+                        # exec script
+                        exec_cmd(current_entry)
+                        # Increment the index for the next iteration
+                        index += 1
+                elif continueBoolean in ["n", "N"]:
+                    print(f"[WARN] {continueBoolean} was selected. Aborting preset and exiting.")
+                else:
+                    print("[WARN] Please select a valid option. Aborting preset and exiting.")
+                print("[INFO] lp has finished running the preset. Now exiting.")
         else:
             print(f"[WARN] arg2 is required for {arg1} to properly function. Now exiting.")
     else:
@@ -57,3 +90,4 @@ if platform.system() == "Linux": # Check if running on Linux, if not, exit.
         main()
 else:
     print("[WARN] lp can only be run on linux-based systems. Now exiting.")
+
