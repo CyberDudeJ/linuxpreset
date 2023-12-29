0148694063905117a8f4680cbda8c159d03dcd72
Ydupc <65360930+CyberDudeJ@users.noreply.github.com>
2023-12-29 14:40:34 +0000

    Add convert json to list

--- lp.py
+++ lp.py
@@ -6,7 +6,25 @@ import sys, os, requests, subprocess, platform
 arg1 = None
 arg2 = None
 
-def main(): # Define main function 
+def fetch_json_data(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status() # Raise an HTTPError for bad responses
+        return response.json()
+    except requests.exceptions.RequestExceptions as e:
+        print(f"[WARN] Error fetching JSON data {e}, Now exiting.")
+        sys.exit(1)
+
+def extract_values(json_data):
+    values_list= []
+
+    for item in json_data:
+        for key, value in item.items():
+            values_list.append(value)
+
+    return values_list
+
+def main(): # Define main function
     global arg1, arg2 # Declare arg1 + arg2 as global variables
 
     # Check if the correct number of arguments has been defined by the user
@@ -22,8 +40,13 @@ def main(): # Define main function
         print("Help coming soon idk")
     elif arg1 in ["r", "run"]:
         if len(sys.argv) > 2: # Check if arg2 exists
-            arg2 = sys.argv[2]
-            print(arg2)
+            arg2 = sys.argv[2] # begin script after getting arg2 again
+            json_data = fetch_json_data(arg2)
+
+            if json_data is not None:
+                values_list = extract_values(json_data)
+                print("Values list:", values_list)
+
         else:
             print(f"[WARN] arg2 is required for {arg1} to properly function. Now exiting.")
     else:
