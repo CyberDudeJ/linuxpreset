2d1bde58d793400f98180b4fe0705d3ec722f2e2
su55y <cesarmilano88@yahoo.com>
2024-01-05 23:01:59 +0200

    naming, type hints

--- lp.py
+++ lp.py
@@ -4,8 +4,10 @@ import argparse
 import json
 import logging
 from pathlib import Path
-import sys, requests, subprocess, platform
-from typing import Any
+import platform
+import requests
+import subprocess as sp
+from typing import Any, List
 
 
 logging.basicConfig(
@@ -16,46 +18,34 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 
 
-# Define command execution function
 def exec_cmd(command: str) -> int:
     try:
-        return subprocess.run(command.split()).returncode
+        return sp.run(command.split()).returncode
     except Exception as e:
         log.error(e)
         return -1
 
 
-# Define check if cmd exists function
-# def command_exists(command):
-#     try:
-#         subprocess.check_output(["which", command])
-#         return True
-#     except subprocess.CalledProcessError:
-#         return False
-
-
-# Define fetch json file function
-def fetch_json_data(url) -> Any:
+def fetch_preset(url: str) -> Any:
     try:
         response = requests.get(url)
         response.raise_for_status()  # Raise an HTTPError for bad responses
         return response.json()
     except requests.exceptions.RequestException as e:
         log.error("Error fetching JSON data: %s, Now exiting.", e)
-        sys.exit(1)
-
-
-# read preset from local json file
-def read_json_data(path: Path) -> Any:
-    try:
-        with open(path) as f:
-            return json.load(f)
-    except:
         exit(1)
 
 
-# json validation func
-def validate_json_data(data) -> bool:
+def read_preset(path: str) -> Any:
+    try:
+        with open(path) as f:
+            return json.load(f)
+    except Exception as e:
+        log.error("Error reading JSON data: %s", e)
+        exit(1)
+
+
+def validate_json_data(data: Any) -> bool:
     return isinstance(data, list) and all(isinstance(v, str) for v in data)
 
 
@@ -65,40 +55,42 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def main():  # Define main function
-    if platform.system() != "Linux":  # Check if running on Linux, if not, exit.
+def run_preset(preset: List[str]):
+    for i, cmd in enumerate(preset):
+        log.info(f"[{i+1}/{len(preset)}] {cmd!r} running...")
+        status = exec_cmd(cmd)
+        if status != 0:
+            log.warning(f"cmd {cmd!r} failed. Exit status {status}")
+
+
+def main():
+    if platform.system() != "Linux":
         log.error("lp can only be run on linux-based systems. Now exiting.")
         exit(1)
 
     args = parse_args()
-
-    # Extract values list
     if Path(args.url).exists():
-        values_list = read_json_data(args.url)
+        preset = read_preset(args.url)
     else:
-        values_list = fetch_json_data(args.url)
+        preset = fetch_preset(args.url)
 
-    # Validate data
-    if not validate_json_data(values_list):
+    if not validate_json_data(preset):
         log.error("Invalid json format, should be list[str]")
         exit(1)
 
     log.info(
         "Selected Preset:\n"
-        + "\n".join(f"  {v!r}" for v in values_list)
+        + "\n".join(f"  {v!r}" for v in preset)
         + "\nSome presets require root. Errors may occur if lp is not run as root"
     )
-    continueBoolean = input("Would you like to continue? [y/n]")
-    if not any(continueBoolean == y for y in ["y", "Y"]):
-        log.info(f"{continueBoolean!r} was selected. Aborting preset and exiting.")
+
+    resp = input("Would you like to continue? [y/n]")
+    if not any(resp == y for y in ["y", "Y"]):
+        log.info(f"{resp!r} was selected. Aborting preset and exiting.")
         exit(0)
 
     log.info("This may take some time depending on the preset. Please wait")
-    for cmd in values_list:
-        log.info(f"{cmd!r} running...")
-        status = exec_cmd(cmd)
-        if status != 0:
-            log.warning(f"cmd {cmd!r} failed. Exit status {status}")
+    run_preset(preset)
     log.info("lp has finished running the preset. Now exiting.")
 
 
